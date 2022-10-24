6fb2af036240af0757e635e9b7365c5ee8e6e633
Jameson Yee <yeeja@oregonstate.edu>
2022-10-24 01:06:42 -0700

    nav bar
    started nav bar and added local server hosting on port 6969

--- server.js
+++ server.js
@@ -1,6 +1,22 @@
-var http = require('http');
+var path = require('path');
+var express = require('express');
+var xpbar = require('express-handlebars');
 
-var port = process.env.PORT || 69420;
+var app  = express();
+var port = process.env.PORT || 6969;
+
+app.get('/', function (req,res) {
+    res.status(200).sendFile(path.join(__dirname, '/Public/index.html'))
+});
+
+/*
+app.get('*', function (req, res) {
+    //res.status(404).sendFile(path.join(__dirname, 'public', '404.html'));
+    res.status(404).render('404', {
+      url: req.url
+    })
+  });
+*/
 
 app.listen(port, function () {
     console.log("== Server is listening on port", port);
