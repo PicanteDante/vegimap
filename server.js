a593d5abfc3b7503e5ea7a34c35c3372db71ead5
Dante <notapirate69yarhar@gmail.com>
2022-10-26 20:01:21 -0700

    did some shit

--- server.js
+++ server.js
@@ -1,13 +1,35 @@
 var path = require('path');
 var express = require('express');
-
+var exphbs = require('express-handlebars')
 var app  = express();
 var port = process.env.PORT || 6969;
 
-app.use(express.static(path.join(__dirname, 'Public')));
+//app.use(express.static(path.join(__dirname, 'Public')));
+app.use(express.json())
+app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+app.use(express.static('Public'))
 
-app.get('/', function (req,res) {
-    res.status(200).sendFile(path.join(__dirname, '/Public/index.html'))
+
+app.get('/', function (req, res, next) {
+	if (req)
+	{
+		//res.status(200).sendFile(path.join(__dirname, '/Public/index.html'))
+		res.status(200).render('map')
+	}
+	else
+	{
+		next();
+	}
+});
+
+
+
+
+app.get('*', function (req, res) {
+  res.status(404).render('404', {
+
+  });
 });
 
 /*
@@ -20,5 +42,8 @@ app.get('*', function (req, res) {
 */
 
 app.listen(port, function () {
+	if (err){
+		throw err
+	}
     console.log("== Server is listening on port", port);
 });
