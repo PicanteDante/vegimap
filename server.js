3a0b48159fbfc53f48bbb593b77fba808528915e
Dante <notapirate69yarhar@gmail.com>
2022-10-26 21:41:39 -0700

    removed local leaflet

--- server.js
+++ server.js
@@ -11,12 +11,6 @@ app.set('view engine', 'handlebars')
 app.use(express.static('Public'))
 
 
-//var map = L.map('map').setView([51.505, -0.09], 13);
-//L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-//	maxZoom: 19,
-//	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-//}).addTo(map);
-
 
 app.get('/', function (req, res, next) {
 	if (req)
