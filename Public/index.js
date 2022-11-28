1fa7bef1837cab7e748201c4bfa4ac0beadbed50
Jameson Yee <yeeja@oregonstate.edu>
2022-11-28 02:08:11 -0800

    clear modal text function

--- Public/index.js
+++ Public/index.js
@@ -39,9 +39,27 @@ function hideAddSomethingModal() {
 
   showSomethingModal.classList.add('hidden');
   modalBackdrop.classList.add('hidden');
+
+  clearAddSomethingModalInputs();
 }
 
 var modalHideButtons = document.getElementsByClassName('modal-hide-button');
 for (var i = 0; i < modalHideButtons.length; i++) {
   modalHideButtons[i].addEventListener('click', hideAddSomethingModal);
+}
+
+function clearAddSomethingModalInputs() {
+
+  var postTextInputElements = [
+    document.getElementById('marker-name-input'),
+    document.getElementById('marker-photo-input'),
+    document.getElementById('marker-desc-input')
+  ];
+
+  /*
+   * Clear any text entered in the text inputs.
+   */
+  postTextInputElements.forEach(function (inputElem) {
+    inputElem.value = "";
+  });
 }
