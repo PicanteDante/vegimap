ec8798fa397cb8d1e40a03b165eb535e05debac5
Dante <notapirate69yarhar@gmail.com>
2022-12-01 18:16:43 -0800

    fixed tabbing on addbutton.js

--- Public/scripts/addbutton.js
+++ Public/scripts/addbutton.js
@@ -2,49 +2,49 @@
 
 function showAddSomethingModal() {
 
-    var showSomethingModal = document.getElementById('add-something-modal');
+	var showSomethingModal = document.getElementById('add-something-modal');
     var modalBackdrop = document.getElementById('modal-backdrop');
   
     showSomethingModal.classList.remove('hidden');
     modalBackdrop.classList.remove('hidden');
-  }
+}
   
-  window.addEventListener('DOMContentLoaded', function () {
-    var addSomethingButton = document.getElementById('add-plant-button');
-  
-    if (addSomethingButton) {
-      addSomethingButton.addEventListener('click', showAddSomethingModal);
-    }
-  });
-  
-  function hideAddSomethingModal() {
-  
-    var showSomethingModal = document.getElementById('add-something-modal');
-    var modalBackdrop = document.getElementById('modal-backdrop');
-  
-    showSomethingModal.classList.add('hidden');
-    modalBackdrop.classList.add('hidden');
-  
-    clearAddSomethingModalInputs();
-  }
-  
-  var modalHideButtons = document.getElementsByClassName('modal-hide-button');
-  for (var i = 0; i < modalHideButtons.length; i++) {
-    modalHideButtons[i].addEventListener('click', hideAddSomethingModal);
-  }
-  
-  function clearAddSomethingModalInputs() {
-  
-    var postTextInputElements = [
-      document.getElementById('marker-name-input'),
-      document.getElementById('marker-photo-input'),
-      document.getElementById('marker-desc-input')
-    ];
-  
-    /*
-     * Clear any text entered in the text inputs.
-     */
-    postTextInputElements.forEach(function (inputElem) {
-      inputElem.value = "";
-    });
-  }
+window.addEventListener('DOMContentLoaded', function () {
+var addSomethingButton = document.getElementById('add-plant-button');
+
+if (addSomethingButton) {
+	addSomethingButton.addEventListener('click', showAddSomethingModal);
+	}
+});
+
+function hideAddSomethingModal() {
+
+	var showSomethingModal = document.getElementById('add-something-modal');
+	var modalBackdrop = document.getElementById('modal-backdrop');
+
+	showSomethingModal.classList.add('hidden');
+	modalBackdrop.classList.add('hidden');
+
+	clearAddSomethingModalInputs();
+}
+
+var modalHideButtons = document.getElementsByClassName('modal-hide-button');
+	for (var i = 0; i < modalHideButtons.length; i++) {
+		modalHideButtons[i].addEventListener('click', hideAddSomethingModal);
+	}
+
+function clearAddSomethingModalInputs() {
+
+	var postTextInputElements = [
+		document.getElementById('marker-name-input'),
+		document.getElementById('marker-photo-input'),
+		document.getElementById('marker-desc-input')
+	];
+
+	/*
+	 * Clear any text entered in the text inputs.
+	 */
+	postTextInputElements.forEach(function (inputElem) {
+		inputElem.value = "";
+	});
+}
