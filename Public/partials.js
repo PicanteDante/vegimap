88522fa3f4dd7db55c533d0895a7ee2f1c4fcbc5
Jameson Yee <yeeja@oregonstate.edu>
2022-12-06 17:16:22 -0800

    nav bar

--- Public/partials.js
+++ Public/partials.js
@@ -48,11 +48,11 @@ templates['onePost'] = template({"compiler":[8,">= 4.3.0"],"main":function(conta
         return undefined
     };
 
-  return "<main id=\"workspace\">\n    <div class='title-contents'>\n        <div class='title'>\n            <a>"
+  return "<main id=\"workspace\">\r\n    <div class='title-contents'>\r\n        <div class='title'>\r\n            <a>"
     + alias4(((helper = (helper = lookupProperty(helpers,"title") || (depth0 != null ? lookupProperty(depth0,"title") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"title","hash":{},"data":data,"loc":{"start":{"line":4,"column":15},"end":{"line":4,"column":24}}}) : helper)))
-    + "</a>\n        </div>\n        <div class='title-username'>\n            <a>"
+    + "</a>\r\n        </div>\r\n        <div class='title-username'>\r\n            <a>"
     + alias4(((helper = (helper = lookupProperty(helpers,"username") || (depth0 != null ? lookupProperty(depth0,"username") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"username","hash":{},"data":data,"loc":{"start":{"line":7,"column":15},"end":{"line":7,"column":27}}}) : helper)))
-    + "</a>\n        </div>\n    </div>\n</main>";
+    + "</a>\r\n        </div>\r\n    </div>\r\n</main>";
 },"useData":true});
 templates['rankedUser'] = template({"compiler":[8,">= 4.3.0"],"main":function(container,depth0,helpers,partials,data) {
     var helper, alias1=depth0 != null ? depth0 : (container.nullContext || {}), alias2=container.hooks.helperMissing, alias3="function", alias4=container.escapeExpression, lookupProperty = container.lookupProperty || function(parent, propertyName) {
@@ -82,18 +82,18 @@ templates['userProfile'] = template({"compiler":[8,">= 4.3.0"],"main":function(c
         return undefined
     };
 
-  return "<div class=\"top-half\"></div>\n    <a class=\"avatar\" id=\"profile-picture\" href=\""
+  return "<div class=\"top-half\"></div>\r\n    <a class=\"avatar\" id=\"profile-picture\" href=\""
     + alias4(((helper = (helper = lookupProperty(helpers,"profileUrl") || (depth0 != null ? lookupProperty(depth0,"profileUrl") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"profileUrl","hash":{},"data":data,"loc":{"start":{"line":2,"column":49},"end":{"line":2,"column":63}}}) : helper)))
-    + "\">\n        <img src=\"../"
+    + "\">\r\n        <img src=\"../"
     + alias4(((helper = (helper = lookupProperty(helpers,"pfpUrl") || (depth0 != null ? lookupProperty(depth0,"pfpUrl") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"pfpUrl","hash":{},"data":data,"loc":{"start":{"line":3,"column":21},"end":{"line":3,"column":31}}}) : helper)))
-    + "\">\n    </a>        \n    <div class=\"profile-username\"> \n        <a class=\"username\" href=\"/users/"
+    + "\">\r\n    </a>        \r\n    <div class=\"profile-username\"> \r\n        <a class=\"username\" href=\"/users/"
     + alias4(((helper = (helper = lookupProperty(helpers,"username") || (depth0 != null ? lookupProperty(depth0,"username") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"username","hash":{},"data":data,"loc":{"start":{"line":6,"column":41},"end":{"line":6,"column":53}}}) : helper)))
     + "\">"
     + alias4(((helper = (helper = lookupProperty(helpers,"username") || (depth0 != null ? lookupProperty(depth0,"username") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"username","hash":{},"data":data,"loc":{"start":{"line":6,"column":55},"end":{"line":6,"column":67}}}) : helper)))
-    + "\n        <div class=\"date-joined\"> Joined "
+    + "\r\n        <div class=\"date-joined\"> Joined "
     + alias4(((helper = (helper = lookupProperty(helpers,"dateJoined") || (depth0 != null ? lookupProperty(depth0,"dateJoined") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"dateJoined","hash":{},"data":data,"loc":{"start":{"line":7,"column":41},"end":{"line":7,"column":55}}}) : helper)))
-    + "</div></a>\n    </div>\n</div>\n<div class=\"bottom-half\">\n    <br><br><br><br>\n    <div class=\"points\" id=\"profile-points\">"
+    + "</div></a>\r\n    </div>\r\n</div>\r\n<div class=\"bottom-half\">\r\n    <br><br><br><br>\r\n    <div class=\"points\" id=\"profile-points\">"
     + alias4(((helper = (helper = lookupProperty(helpers,"profilePoints") || (depth0 != null ? lookupProperty(depth0,"profilePoints") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"profilePoints","hash":{},"data":data,"loc":{"start":{"line":12,"column":44},"end":{"line":12,"column":61}}}) : helper)))
-    + "</div>\n    <br><br><br>\n    <a class=\"user-markers\" href=\"#\"> Your Markers</a>\n</div>";
+    + "</div>\r\n    <br><br><br>\r\n    <a class=\"user-markers\" href=\"#\"> Your Markers</a>\r\n</div>";
 },"useData":true});
 })();
